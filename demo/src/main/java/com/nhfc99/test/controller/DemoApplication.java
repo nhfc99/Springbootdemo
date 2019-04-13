0f070070b7a23a3b71aa679e4076fce1e622f68f
nhf <nhfc99@163.com>
2019-04-13 19:32:00 +0800

    优化

--- demo/src/main/java/com/nhfc99/test/controller/DemoApplication.java
+++ demo/src/main/java/com/nhfc99/test/controller/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.nhfc99.demo;
+package com.nhfc99.test.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,9 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
